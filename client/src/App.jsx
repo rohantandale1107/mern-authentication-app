547c063a95c585d5a2b1d8dcb2f533feeca989ba
rohantandale1107 <rohanhtandale11@gmail.com>
2024-06-27 23:52:14 +0530

    added routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home"
 
 export default function App() {
   return (
-    <div>
-      app
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
