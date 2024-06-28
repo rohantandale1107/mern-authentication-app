3328eaa6cc563e837d7e9aa58684b780f434f809
rohantandale1107 <rohanhtandale11@gmail.com>
2024-06-28 08:24:36 +0530

    mongodb connection establish

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.log("Errorconnecting to MongoDB:", err.message));
 
 const app = express();
 
